367d590d5dad4a988a8686a215c7d97a939994b7
Sara1812 <sarabea18@gmail.com>
2019-05-13 18:07:21 -0400

    Se hizo una adivinanza

--- adivinaza.py
+++ adivinaza.py
@@ -1,3 +1,22 @@
 """
 juego de adivinar el numero, todos juntos adivinar un numero generado aleatoriamente e ir introducciendo
 por teclado el dato a adivinar
+
+"""
+from random import randint
+generado=randint(0,10)
+print(generado)
+condicion=True
+intento=10
+while condicion:
+    if intento>0:
+        numero=intento-1
+        if generado==int(numero):
+            print("Ganaste una anvorguesa que Lore paga")
+            condicion=False
+        else:
+            print("El perdedor compra pizza a Lore")
+            print("Te quedan",intento"intentos")
+        else:
+            condicion=False
+            print("perdiste")
