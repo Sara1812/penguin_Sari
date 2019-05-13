9e9be705995bbc0d88b48c30ef6f0e2b874ec3ef
Sara1812 <sarabea18@gmail.com>
2019-05-13 18:03:18 -0400

    Se hizo una adivinanza

---
+++ adivinaza.py
@@ -0,0 +1,3 @@
+"""
+juego de adivinar el numero, todos juntos adivinar un numero generado aleatoriamente e ir introducciendo
+por teclado el dato a adivinar
--- dia73.py
+++ dia73.py
@@ -15,15 +15,17 @@ print(aleatorio)
 #escribir una funcion sorteo() que reciba una lista de participantes y elegir a uno de los participantes
 #aleatoriamente y retornar esa persona aelgida
 #desafio: no volver a retornar una persona ya sorteada
-"""
-from random import randint   #
-def sorteo(lista):
-    cant_part=len(lista)
-    ganadores=randint(0,cant_part-1)
+
+#importamos la funcion randit
+from random import randint   
+def sorteo(lista1):         #definimos una funcion
+    cant_part=len(lista)-1    #utilizamos len para contar la cantidad de personas que hay en (lista) y guardamos en la variable cant y le restamos
+    ganadores=randint(0,cant_part)
     return lista[ganadores]
     print(lista[ganadores])
 
 lista=["Vale","Kami","Sara","Ale","Dulce"]
+ganar=sorteo(lista)
 sorteo(lista)
 
 """
